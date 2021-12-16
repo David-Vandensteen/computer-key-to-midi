b29b1c58fa524d7c0495046eddf4f592c1ea5f73
David Vandensteen <david.vandensteen@leroymerlin.fr>
2021-12-16 14:00:48 +0100

    First

---
+++ src/config/default-fr.js
@@ -0,0 +1,31 @@
+export default {
+  keys: [
+    {
+      key: '&',
+      increment: 1,
+      cc: 1,
+    },
+    {
+      key: 'a',
+      increment: -1,
+      cc: 1,
+    },
+    {
+      key: 'é',
+      increment: 1,
+      cc: 2,
+    },
+    {
+      key: 'z',
+      increment: -1,
+      cc: 2,
+    },
+  ],
+  ccs: {
+    1: {
+      value: 64,
+      max: 100,
+      min: 5,
+    },
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import MidiKey from '#src/lib/midikey';
+
+const midiKey = new MidiKey();
+
+midiKey
+  .register(1)
+  .start();
