1860dfa6c8102ce18eebafe51f5243eafcf8de55
David Vandensteen <dvandensteen@gmail.com>
2023-01-15 15:11:43 +0100

    Add : cli examples

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,7 +8,12 @@ export default (file) => {
   if (!existsSync(file) || file === undefined) {
     log('no config provided, try to load a fallback config mcc.yaml');
     configFile = 'mcc.yaml';
+    if (!existsSync(configFile)) {
+      configFile = 'src/model/mcc-1/config/default-fr.yaml';
+      log('mcc.yaml config was not found, try to load', configFile);
+    }
   }
+  if (!existsSync(configFile)) throw new Error(configFile, 'not found');
   const config = readFileSync(configFile, 'utf8');
   return YAML.parse(config);
 };
--- src/service/paramService.js
+++ src/service/paramService.js
@@ -20,6 +20,12 @@ const help = () => {
   log('   -i    --interface-in        -- midi in interface name');
   log('   --list     -l               -- show available midi interfaces');
   log('   --help                      -- show help');
+  log('');
+  log('master example :');
+  log('mcc -m master -i mcc-in -o mcc-out -h 0.0.0.0 -p 7070');
+  log('');
+  log('slave example :');
+  log('mcc -m slave -h 192.168.0.1 -p 7070');
   process.exit(0);
 };
 
