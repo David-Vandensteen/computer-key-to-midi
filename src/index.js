c3e758d292d9ea5e9cf9ecbb3e5ecf1168b49f97
David Vandensteen <dvandensteen@gmail.com>
2023-01-22 12:29:27 +0100

    Fix : move application config to src/config

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import { ApplicationConfigService } from '#src/service/applicationConfigService'
 import { argService } from '#src/service/argService';
 import { ApplicationService } from '#src/service/applicationService';
 
-const config = ApplicationConfigService.get({ applicationConfigFile: 'config/application.yaml', argService });
+const config = ApplicationConfigService.get({ applicationConfigFile: 'src/config/application.yaml', argService });
 ApplicationService.run(config);
