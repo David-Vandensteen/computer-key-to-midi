b755a01c1b899ae09a58c0311d63df6d64ea3ff9
David Vandensteen <dvandensteen@gmail.com>
2023-04-15 16:07:04 +0200

    Remove : useless commented code

--- src/lib/YAMLReader.js
+++ src/lib/YAMLReader.js
@@ -37,26 +37,3 @@ export default (yamlFile, options) => {
     throw new Error('No YAML file was founded');
   }
 };
-
-/*
-
-export default class ConfigReader {
-  static getAvailableFile(configPaths) {
-    const processedConfigPaths = Array.isArray(configPaths) ? configPaths : [configPaths];
-    processedConfigPaths.map((configPath) => {
-      processedConfigPaths.push(resolve(configPath));
-      return configPath;
-    });
-    const foundPath = processedConfigPaths.find((path) => existsSync(path) === true);
-    return foundPath;
-  }
-
-  static get(configPaths) {
-    log.title('Loading config file...');
-    const foundPath = ConfigReader.getAvailableFile(configPaths);
-    if (foundPath) return getFromFile(foundPath);
-
-    throw new Error('No config file found');
-  }
-}
-*/
